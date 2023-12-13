2ff3951821e666f5f97dac6d648f459b2cfb3cea
Luka <majstorovicluka20@gmail.com>
2023-12-13 13:21:24 +0100

    Spring inicijalizacija in properties.

---
+++ whisperlink/src/test/java/com/whisperlink/whisperlink/WhisperlinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whisperlink.whisperlink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhisperlinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
