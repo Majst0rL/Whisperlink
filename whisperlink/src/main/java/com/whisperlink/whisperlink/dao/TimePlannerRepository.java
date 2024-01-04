6a8bb32bfbdefc427e9c29b14df3b354fc03513e
Mojca <mojca.marin@student.um.si>
2024-01-04 23:20:08 +0100

    Implementirana dvakrat bolj kompleksna poizvedba za nalogo WP-16 (US-16).
    Dokončana naloga #WP-16.
    Porabljen čas: 15 minut.

--- whisperlink/src/main/java/com/whisperlink/whisperlink/dao/TimePlannerRepository.java
+++ whisperlink/src/main/java/com/whisperlink/whisperlink/dao/TimePlannerRepository.java
@@ -6,7 +6,9 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
+
 
 @Repository
 public interface TimePlannerRepository extends CrudRepository<TimePlanner, Long> {
@@ -15,5 +17,9 @@ public interface TimePlannerRepository extends CrudRepository<TimePlanner, Long>
     // Complex query with 2 parameters
     @Query("SELECT t FROM TimePlanner t WHERE t.location = :location AND t.name = :name")
     List<TimePlanner> findByLocationAndName(@Param("location") String location, @Param("name") String name);
+
+    //  complex query with 2 parameters (start and end date)
+    @Query("SELECT t FROM TimePlanner t WHERE t.date BETWEEN :startDate AND :endDate")
+    List<TimePlanner> findByDateBetween(@Param("startDate") LocalDate startDate, @Param("endDate") LocalDate endDate);
 
 }
