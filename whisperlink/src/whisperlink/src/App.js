4ea15684b49675c10cf47cbc5ecee1f0a19ad398
Luka <majstorovicluka20@gmail.com>
2024-01-23 20:45:47 +0100

    Remoed password encoding

--- whisperlink/src/whisperlink/src/App.js
+++ whisperlink/src/whisperlink/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import Gender from "./components/Gender.jsx";
 import DateCalendar from "./components/DateCalendar.jsx";
 import Location from "./components/Location.jsx";
